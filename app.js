1d19192bb2864d7450b21de630f25ee59410fda3
fal4782 <falguni4782@gmail.com>
2023-09-08 19:04:07 +0530

    basic html and css

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const time = moment().format('MMMM Do YYYY, h:mm:ss a')
+const time = moment().format('MMMM, YYYY')
 
 console.log(time);
 
-document.getElementById("timestamp").innerHTML = time
+// document.getElementById("timestamp").innerHTML = time
+
+for(let i = 1; i<=12; i++){
+    let noOfDays = moment(i,'M').daysInMonth()
+    console.log(noOfDays);
+}
