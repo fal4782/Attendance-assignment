2c9745e09c1e335e4785e8cd77923ba454951320
fal4782 <falguni4782@gmail.com>
2023-09-09 13:27:05 +0530

    added dates to every month

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ const time = moment().format('MMMM, YYYY')
 console.log(time);
 
 // document.getElementById("timestamp").innerHTML = time
+const year = 2016
+for(let month = 1; month<=12; month++){
+    let monthName = moment().month(month).format('MMMM')
+    let monthFormat = month +", "+year
+    // console.log(monthFormat);
+    let noOfDays = moment(monthFormat,'M, YYYY').daysInMonth()
+    // console.log(noOfDays);
 
-for(let i = 1; i<=12; i++){
-    let noOfDays = moment(i,'M').daysInMonth()
-    console.log(noOfDays);
+    const monthBox = document.getElementById(monthName.toLowerCase())
+    // console.log(monthBox);
+    let dateContent = ''
+
+    for(let day=1;day<=noOfDays;day++){
+        // dateContent += `<div class="dateBox">${day}</div>`
+        dateContent += `<label for="vehicle1">${day}</label><input type="checkbox" value="${day}">`
+    }
+
+    monthBox.innerHTML = dateContent
 }
