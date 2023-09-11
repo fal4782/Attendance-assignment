eb7275ff9423df147ff77a1fd3d6e627ff19f824
fal4782 <falguni4782@gmail.com>
2023-09-11 10:00:21 +0530

    added dayOfWeek and visibility settings

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 
 function setYear() {
-    var year = document.getElementById("year").value
+
+    var year = document.getElementById("year").value || 2016
     console.log(year);
 
-for(let month = 0; month<=11; month++){
-    let monthName = moment().month(month).format('MMMM')
-    let monthFormat = (month+1) +", "+year
-    // console.log(monthFormat);
+    document.querySelector(".parent-container").style.display="block"
+    
+    for (let month = 0; month <= 11; month++) {
+        let monthName = moment().month(month).format('MMMM')
+        let monthFormat = (month + 1) + ", " + year
+        // console.log(monthFormat);
 
-    let noOfDays = moment(monthFormat,'M, YYYY').daysInMonth()
-    console.log(noOfDays, monthName);
+        let noOfDays = moment(monthFormat, 'M, YYYY').daysInMonth()
+        console.log(noOfDays, monthName);
 
-    const monthBox = document.getElementById(monthName.toLowerCase())
-    // console.log(monthBox);
-    let dateContent = ''
+        const monthBox = document.getElementById(monthName.toLowerCase())
+        // console.log(monthBox);
+        let dateContent = ''
 
-    for(let day=1;day<=noOfDays;day++){
-        dateContent += `<div class = "dateWrapper"><label for="day">${day}</label><input type="checkbox" value="${day}"></div>`
+        for (let day = 1; day <= noOfDays; day++) {
+
+            let date = moment(`${day}-${month+1}-${year}`, 'D-M-YYYY')
+            let dayOfWeek = date.format('ddd')
+
+            dateContent += `<div class = "dateWrapper"><label for="day">${day}</label><input type="checkbox" value="${day}"><label for="day" id="weekDay">${dayOfWeek}</label></div>`
+        }
+
+        monthBox.innerHTML = dateContent
     }
-
-    monthBox.innerHTML = dateContent
-}
     // return year
 }
 
